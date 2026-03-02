8d48aa301eef952692b699a38b213bb7c5546a26
R.Ragulrathinasamy <ragul7122003@gmail.com>
2026-03-02 13:41:23 +0530

    Add files via upload

--- user.php
+++ user.php
@@ -4,10 +4,11 @@
 <?php include('navbar.php'); ?>
 <div class="container">
 	<div class="row">
-		<?php include('chatlist.php'); ?>
+		<?php include('userlist.php'); ?>
 	</div>
 </div>
-<?php include('crudroom_modal.php'); ?>
+
+<?php include('cruduser_modal.php'); ?>
 <?php include('modal.php'); ?>
 
 <script src="../js/jquery.dataTables.min.js"></script>
@@ -16,7 +17,8 @@
 <script>
 $(document).ready(function(){
 	
-	$('#chatRoom').DataTable({
+	
+	$('#userList').DataTable({
 	"bLengthChange": true,
 	"bInfo": true,
 	"bPaginate": true,
@@ -25,80 +27,90 @@ $(document).ready(function(){
 	"pageLength": 7
 	});
 	
-	$(document).on('click', '#addchatroom', function(){
-		chatname=$('#chat_name').val();
-		chatpass=$('#chat_password').val();
+	$(document).on('click', '#adduser', function(){
+		name=$('#uname').val();
+		username=$('#uusername').val();
+		password=$('#upassword').val();
+		access=$('#uaccess').val();
 			$.ajax({
-				url:"add_chatroom.php",
+				url:"add_user.php",
 				method:"POST",
 				data:{
-					chatname: chatname,
-					chatpass: chatpass,
+					name: name,
+					username: username,
+					password: password,
+					access: access,
+					adduser: 1,
 				},
-				success:function(data){
-				window.location.href='chatroom.php?id='+data;
+				success:function(){
+					window.location.href='user.php';
 				}
 			});
 		
 	});
 	//
-	$(document).on('click', '.delete', function(){
+	$(document).on('click', '.deleteuser', function(){
 		var rid=$(this).val();
-		$('#delete_room').modal('show');
+		$('#delete_user').modal('show');
 		$('.modal-footer #confirm_delete').val(rid);
 	});
 	
 	$(document).on('click', '#confirm_delete', function(){
 		var nrid=$(this).val();
-		$('#delete_room').modal('hide');
+		$('#delete_user').modal('hide');
 		$('body').removeClass('modal-open');
 		$('.modal-backdrop').remove();
 			$.ajax({
-				url:"deleteroom.php",
+				url:"deleteuser.php",
 				method:"POST",
 				data:{
 					id: nrid,
 					del: 1,
 				},
 				success:function(){
-					window.location.href='index.php';
+					window.location.href='user.php';
 				}
 			});
 	});
 	
-	$(document).on('click', '.edit', function(){
+	$(document).on('click', '.edituser', function(){
 		var rid=$(this).val();
-		var name=$('#name'+rid).val();
-		var pass=$('#pass'+rid).val();
-		$('#edit_room').modal('show');
-		$('.modal-body #room_name').val(name);
-		$('.modal-body #room_password').val(pass);
+		var name=$('#ename'+rid).val();
+		var username=$('#eusername'+rid).val();
+		var password=$('#epassword'+rid).val();
+		$('#edit_user').modal('show');
+		$('.modal-body #user_name').val(name);
+		$('.modal-body #user_user').val(username);
+		$('.modal-body #user_pass').val(password);
 		$('.modal-footer #confirm_update').val(rid);
 	});
 	
 	$(document).on('click', '#confirm_update', function(){
 		var nrid=$(this).val();
-		var roomname=$('#room_name').val();
-		var roompass=$('#room_password').val();
-		$('#edit_room').modal('hide');
+		var nname=$('#user_name').val();
+		var nuser=$('#user_user').val();
+		var npass=$('#user_pass').val();
+		$('#edit_user').modal('hide');
 		$('body').removeClass('modal-open');
 		$('.modal-backdrop').remove();
 			$.ajax({
-				url:"update_room.php",
+				url:"update_user.php",
 				method:"POST",
 				data:{
 					id: nrid,
-					name: roomname,
-					pass: roompass,
+					name: nname,
+					username: nuser,
+					password: npass,
 					edit: 1,
 				},
 				success:function(){
-					window.location.href='index.php';
+					window.location.href='user.php';
 				}
 			});
 	});
  
 });
+
 </script>	
 </body>
 </html>
